603b38ef175e96ef6c54fd397b79e8c7167b4a26
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2024-12-19 18:30:02 +0800

    Release 241219

--- tests/test_biocjson_parser.py
+++ tests/test_biocjson_parser.py
@@ -1,54 +1,55 @@
-import pytest
-from pubtoscape.biocjson_parser import convert_to_pubtator
-from pathlib import Path
 import json
+from pathlib import Path
 
-TESTDATA_DIR = Path(__file__).parent / "test_data"
+import pytest
+
+from netmedex.biocjson_parser import biocjson_to_pubtator
 
 
 @pytest.fixture(scope="module")
-def paths():
-    filepaths = {
-        "abstract_json": TESTDATA_DIR / "pubtator3.22439397_abstract_240916.json",
-        "full_json": TESTDATA_DIR / "pubtator3.22429397_full_240916.json",
-        "abstract_pubtator": TESTDATA_DIR / "22429397_abstract_240916.pubtator",
-        "full_pubtator": TESTDATA_DIR / "22429397_full_240916.pubtator",
-        "full_to_abstract_pubtator": TESTDATA_DIR / "22429397_full_to_abstract_240916.pubtator",
+def paths(request):
+    test_dir = Path(request.config.rootdir) / "tests/test_data"
+    paths = {
+        "abstract_json": test_dir / "pubtator3.22439397_abstract_240916.json",
+        "full_json": test_dir / "pubtator3.22429397_full_240916.json",
+        "abstract_pubtator": test_dir / "22429397_abstract_240916.pubtator",
+        "full_pubtator": test_dir / "22429397_full_240916.pubtator",
+        "full_to_abstract_pubtator": test_dir / "22429397_full_to_abstract_240916.pubtator",
     }
-    return filepaths
+    return paths
 
 
 def test_abstract_to_abstract_conversion(paths):
-    with open(paths["abstract_json"], "r") as f:
+    with open(paths["abstract_json"]) as f:
         data = json.load(f)
 
-    result = convert_to_pubtator(data, retain_ori_text=True, only_abstract=True)
+    result = biocjson_to_pubtator(data, retain_ori_text=True, only_abstract=True)
 
-    with open(paths["abstract_pubtator"], "r") as f:
+    with open(paths["abstract_pubtator"]) as f:
         expected = f.read()
 
     assert result == expected
 
 
 def test_full_to_abstract_conversion(paths):
-    with open(paths["full_json"], "r") as f:
+    with open(paths["full_json"]) as f:
         data = json.load(f)
 
-    result = convert_to_pubtator(data, retain_ori_text=True, only_abstract=True)
+    result = biocjson_to_pubtator(data, retain_ori_text=True, only_abstract=True)
 
-    with open(paths["full_to_abstract_pubtator"], "r") as f:
+    with open(paths["full_to_abstract_pubtator"]) as f:
         expected = f.read()
 
     assert result == expected
 
 
 def test_full_conversion(paths):
-    with open(paths["full_json"], "r") as f:
+    with open(paths["full_json"]) as f:
         data = json.load(f)
 
-    result = convert_to_pubtator(data, retain_ori_text=True, only_abstract=False)
+    result = biocjson_to_pubtator(data, retain_ori_text=True, only_abstract=False)
 
-    with open(paths["full_pubtator"], "r") as f:
+    with open(paths["full_pubtator"]) as f:
         expected = f.read()
 
     assert result == expected
--- tests/test_stemmer.py
+++ tests/test_stemmer.py
@@ -1,13 +1,16 @@
 import pytest
 
-from pubtoscape.pubtator3_to_cytoscape_cli import s_stemmer
+from netmedex.stemmers import s_stemmer
 
 
-@pytest.mark.parametrize("input,expected", [
-    ("", ""),
-    ("abdominal diseases", "abdominal disease"),
-    ("reactive oxygen species", "reactive oxygen species"),
-])
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        ("", ""),
+        ("abdominal diseases", "abdominal disease"),
+        ("reactive oxygen species", "reactive oxygen species"),
+    ],
+)
 def test_s_stemmer(input, expected):
     output = s_stemmer(input)
     assert output == expected, f"result: {output}, expected: {expected}"
